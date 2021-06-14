8ab8fb271b04ff30d9503c9b5fbff8315464889e
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-06-14 12:59:38 +0300

    learning javascript

---
+++ javascript/lesson_23/Arrays.js
@@ -0,0 +1,10 @@
+var shoppingList = ["Books","Pen","Pencil", "Eraser", "Sharpener"];
+
+console.log(shoppingList);
+console.log(shoppingList[2]);
+
+var bucketList = ["Morocco", "Cuba", "Kenya", "Tanzania", 
+"South Africa", "Ethiopia"];
+
+console.log(bucketList[1]);
+console.log(bucketList[4])
